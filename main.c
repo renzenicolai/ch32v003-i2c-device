b42b618c19972bfe50e6376d278d01d77dc8b900
Renze Nicolai <renze@rnplus.nl>
2023-05-27 00:23:57 +0200

    Acknowledge first address match event, cleanup

--- main.c
+++ main.c
@@ -6,11 +6,9 @@
 
 #define APB_CLOCK SYSTEM_CORE_CLOCK
 
-uint8_t i2c_registers[16] = {0x00};
+volatile uint8_t i2c_registers[16] = {0x00};
 
 void setup_i2c(uint8_t address) {
-    uint16_t tempreg;
-
     // Enable GPIOC and I2C
     RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
     RCC->APB1PCENR |= RCC_APB1Periph_I2C1;
@@ -27,6 +25,9 @@ void setup_i2c(uint8_t address) {
     RCC->APB1PRSTR |= RCC_APB1Periph_I2C1;
     RCC->APB1PRSTR &= ~RCC_APB1Periph_I2C1;
 
+    I2C1->CTLR1 |= I2C_CTLR1_SWRST;
+    I2C1->CTLR1 &= ~I2C_CTLR1_SWRST;
+
     // Set module clock frequency
     uint32_t prerate = 2000000; // I2C Logic clock rate - must be higher than Bus clock rate
     I2C1->CTLR2 |= (APB_CLOCK/prerate) & I2C_CTLR2_FREQ;
@@ -34,10 +35,11 @@ void setup_i2c(uint8_t address) {
     // Enable interrupts
     I2C1->CTLR2 |= I2C_CTLR2_ITBUFEN;
     I2C1->CTLR2 |= I2C_CTLR2_ITEVTEN; // Event interrupt
-    //I2C1->CTLR2 |= I2C_CTLR2_ITERREN; // Error interrupt
+    I2C1->CTLR2 |= I2C_CTLR2_ITERREN; // Error interrupt
 
     NVIC_EnableIRQ(I2C1_EV_IRQn); // Event interrupt
-    //NVIC_EnableIRQ(I2C1_ER_IRQn); // Error interrupt
+    NVIC_SetPriority(I2C1_EV_IRQn, 2 << 4);
+    NVIC_EnableIRQ(I2C1_ER_IRQn); // Error interrupt
 
     // Set clock configuration
     uint32_t clockrate = 1000000; // I2C Bus clock rate - must be lower the Logic clock rate
@@ -51,99 +53,77 @@ void setup_i2c(uint8_t address) {
     // Enable I2C
     I2C1->CTLR1 |= I2C_CTLR1_PE;
 
-    printf("I2C1->CTLR1  0x%04x\n", I2C1->CTLR1);
-    printf("I2C1->CTLR2  0x%04x\n", I2C1->CTLR2);
-    printf("I2C1->OADDR1 0x%04x\n", I2C1->OADDR1);
-    printf("I2C1->OADDR2 0x%04x\n", I2C1->OADDR2);
-    printf("I2C1->DATAR  0x%04x\n", I2C1->DATAR);
-    printf("I2C1->STAR1  0x%04x\n", I2C1->STAR1);
-    printf("I2C1->STAR2  0x%04x\n", I2C1->STAR2);
-    printf("I2C1->CKCFGR 0x%04x\n", I2C1->CKCFGR);
+    // Acknowledge the first address match event when it happens
+    I2C1->CTLR1 |= I2C_CTLR1_ACK;
 }
 
-uint8_t transaction_length = 0;
-uint8_t transaction_address = 0;
-bool transaction_direction = false;
-bool transaction_initialized = false;
+struct _i2c_state {
+    bool first_write;
+    uint8_t offset;
+    uint8_t position;
+} i2c_state = {
+    .first_write = false,
+    .offset = 0,
+    .position = 0,
+};
 
-bool dbg_start_req = false;
-bool dbg_txe_req = false;
-bool dbg_rxne_req = false;
-bool dbg_init = false;
-bool dbg_af = false;
-bool dbg_stop = false;
-bool dbg_unknown = false;
-
-uint16_t dbg_unknown1, dbg_unknown2;
 
 void I2C1_EV_IRQHandler(void) __attribute__((interrupt));
 void I2C1_EV_IRQHandler(void) {
-    uint16_t STAR1, STAR2, DATAR __attribute__((unused));
+    uint16_t STAR1, STAR2 __attribute__((unused));
     STAR1 = I2C1->STAR1;
     STAR2 = I2C1->STAR2;
 
-    dbg_unknown1 = STAR1;
-    dbg_unknown2 = STAR2;
+    I2C1->CTLR1 |= I2C_CTLR1_ACK;
 
-    if (STAR1 & I2C_STAR1_ADDR) {
-        // Start of transaction
-        transaction_length = 0;
-        transaction_address = 0;
-        transaction_direction = false;
-        transaction_initialized = false;
-        I2C1->CTLR1 |= I2C_CTLR1_ACK; // Ack the start bit
-        dbg_start_req = true;
-    } else if (STAR1 & I2C_STAR1_TXE) {
-        // Read requested
-        if (transaction_address < sizeof(i2c_registers)) {
-            I2C1->DATAR = i2c_registers[transaction_address];
-            transaction_address++;
+    if (STAR1 & I2C_STAR1_ADDR) { // Start event
+        i2c_state.first_write = true; // Next write will be the offset
+        i2c_state.position = i2c_state.offset; // Reset position
+    }
+
+    if (STAR1 & I2C_STAR1_RXNE) { // Write event
+        if (i2c_state.first_write) { // First byte written, set the offset
+            i2c_state.offset = I2C1->DATAR;
+            i2c_state.position = i2c_state.offset;
+            i2c_state.first_write = false;
+        } else { // Normal register write
+            if (i2c_state.position < sizeof(i2c_registers)) {
+                i2c_registers[i2c_state.position] = I2C1->DATAR;
+                i2c_state.position++;
+            }
+        }
+    }
+
+    if (STAR1 & I2C_STAR1_TXE) { // Read event
+        if (i2c_state.position < sizeof(i2c_registers)) {
+            I2C1->DATAR = i2c_registers[i2c_state.position];
+            i2c_state.position++;
         } else {
             I2C1->DATAR = 0x00;
         }
-        I2C1->CTLR1 |= I2C_CTLR1_ACK; // Ack the read
-        dbg_txe_req = true;
-    } else if (STAR1 & I2C_STAR1_RXNE) {
-        // Write requested
-        if (transaction_initialized) {
-            if (transaction_address < sizeof(i2c_registers)) {
-                i2c_registers[transaction_address] = I2C1->DATAR;
-                transaction_address++;
-            }
-            dbg_rxne_req = true;
-        } else {
-            transaction_address = I2C1->DATAR;
-            transaction_initialized = true;
-            dbg_init = true;
-        }
-        I2C1->CTLR1 |= I2C_CTLR1_ACK; // Ack the write
-    } else if (STAR1 & I2C_STAR1_AF) {
-        // Acknowledge failure (end of transaction)
-        I2C1->STAR1 &= ~(I2C_STAR1_AF); // Clear AF bit
-        I2C1->CTLR1 |= I2C_CTLR1_STOP; // Send stop bit
-        dbg_af = true;
-    } else if (STAR1 & I2C_STAR1_STOPF) {
-        I2C1->CTLR1 |= I2C_CTLR1_ACK; // Ack the stop
-        dbg_stop = true;
-    } else {
-        I2C1->CTLR1 |= I2C_CTLR1_STOP; // Send stop bit
-        dbg_unknown = true;
     }
 }
 
-bool error_interrupt_called = false;
-
 void I2C1_ER_IRQHandler(void) __attribute__((interrupt));
 void I2C1_ER_IRQHandler(void) {
-    error_interrupt_called = true;
-    printf("ERROR INTERRUPT\n");
+    uint16_t STAR1 = I2C1->STAR1;
+
+    if (STAR1 & I2C_STAR1_BERR) { // Bus error
+        I2C1->STAR1 &= ~(I2C_STAR1_BERR); // Clear error
+    }
+
+    if (STAR1 & I2C_STAR1_ARLO) { // Arbitration lost error
+        I2C1->STAR1 &= ~(I2C_STAR1_ARLO); // Clear error
+    }
+
+    if (STAR1 & I2C_STAR1_AF) { // Acknowledge failure
+        I2C1->STAR1 &= ~(I2C_STAR1_AF); // Clear error
+    }
 }
 
 int main() {
     SystemInit48HSI();
     SetupDebugPrintf();
-
-    Delay_Ms(100);
 
     setup_i2c(0x9);
 
@@ -155,52 +135,7 @@ int main() {
     GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
 
     while (1) {
-        if (dbg_start_req) {
-            printf("Start\n");
-        }
-
-        if (dbg_txe_req) {
-            printf("Read\n");
-        }
-
-        if (dbg_rxne_req) {
-            printf("Register write, new state: %02x: ", transaction_address);
-            for (uint8_t i = 0; i < sizeof(i2c_registers); i++) {
-                printf("%02x ", i2c_registers[i]);
-            }
-            printf("\n");
-        }
-
-        if (dbg_init) {
-            printf("Init\n");
-        }
-
-        if (dbg_af) {
-            printf("End\n");
-        }
-
-        if (dbg_stop) {
-            printf("Stop\n");
-        }
-
-        if (dbg_unknown) {
-            printf("Unknown %04X %04X\n", dbg_unknown1, dbg_unknown2);
-        }
-
-        dbg_start_req = false;
-        dbg_txe_req = false;
-        dbg_rxne_req = false;
-        dbg_init = false;
-        dbg_af = false;
-        dbg_stop = false;
-        dbg_unknown = false;
-
-        if (error_interrupt_called) {
-            error_interrupt_called = false;
-            printf("Error interrupt called\n");
-        }
-
-        if (i2c_registers[0] & 1) {
+        if (i2c_registers[0] & 1) { // Turn on LED if bit 1 of register 0 is set
             GPIOD-> BSHR |= 1 << 16;
         } else {
             GPIOD-> BSHR |= 1;
